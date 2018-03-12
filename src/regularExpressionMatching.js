65b78541bcf8263723aa9e2ae8d5cd3043962158
zee <zee@nodefe.com>
2018-03-12 11:28:16 +0800

    upd: regular dp solution

--- src/regularExpressionMatching.js
+++ src/regularExpressionMatching.js
@@ -15,3 +15,28 @@ var isMatch = function(s, p) {
     return firstMatch && isMatch(s.substr(1), p.substr(1));
   }
 };
+
+var isMatchDp = function (s, p) {
+  let temp = [];
+  let dp = function (i, j) {
+    if (!(temp[i] && temp[i][j])) {
+      let ret = false,
+          firstMatch = false;
+      temp[i] = [];
+      if (j === p.length) {
+        ret = i === s.length;
+      } else {
+        firstMatch = i < s.length && !!~['.', s[i]].indexOf(p[j]);
+        if (j + 1 < p.length && p[j + 1] === '*') {
+          ret = dp(i, j + 2) || firstMatch && dp(i + 1, j);
+        } else {
+          ret = firstMatch && dp(i + 1, j + 1);
+        }
+      }
+      temp[i][j] = ret;
+    }
+    return  temp[i][j];
+  };
+
+  return dp(0, 0);
+}
